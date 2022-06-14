2c8e3d2481d32c7efa1d654699abc9052cef5cfa
Jaehyuk <jaehyuk.pyon@gmail.com>
2022-06-14 17:31:48 +0900

    Initial commit

---
+++ src/main/java/com/myphoto/MyGooglePhotosReaderApplication.java
@@ -0,0 +1,13 @@
+package com.myphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGooglePhotosReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGooglePhotosReaderApplication.class, args);
+	}
+
+}
